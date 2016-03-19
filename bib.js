5a4dd467522b47b776a6979d80efe39e1f8bb412
Patrick Lacher <jlacher1@nd.edu>
2016-03-19 11:22:09 -0400

    added script to html to open search window on button press

--- bib.js
+++ bib.js
@@ -1,9 +1,4 @@
 
-
-from urllib import FancyURLopener
-class MyOpener(FancyURLopener):
-    version = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36'
-openurl = MyOpener().open
-
-
-openurl(http://scholar.google.se/scholar?hl=en&q=${query}).read()
+	url= "http://scholar.google.se/scholar?hl=en&q=${A+Tale+Of+Two+Cities}"
+	var win = window.open(url, '_blank');
+	win.focus();
